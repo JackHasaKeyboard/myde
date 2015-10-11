9bb920943746bea41b6488e63cc971f1912fb19a
Jack Alma <jackhasakeyboard@gmail.com>
2015-10-10 20:00:10 -0600

    Changed file content type to image/png, testing whether inserting records will work on Heroku

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,4 @@
 class Color < ActiveRecord::Base
 	has_attached_file :file
-	validates_attachment_content_type :file, :content_type => ["application/xml"]
+	validates_attachment_content_type :file, :content_type => ["image/png"]
 end
