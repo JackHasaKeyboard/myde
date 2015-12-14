be2116966206147da11a8e0675c2b7406bfe1931
jackhasakeyboard <jackhasakeyboard@gmail.com>
2015-12-14 08:11:46 -0700

    Added color scheme files in asset folder, removed color model logic

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -1,34 +1,5 @@
 class ColorsController < ApplicationController
-	before_action :find_color, only: [:destroy]
-
 	def index
-		@colors = Color.all.order("file_file_name ASC")
-		@color = Color.new
-
 		@code = Code.first
-	end
-
-	def create
-		@color = Color.new(color_params)
-
-		if @color.save
-			redirect_to #
-		else
-			render 'new'
-		end
-	end
-
-	def destroy
-		@color.destroy
-	end
-
-	private
-
-	def find_color
-		@color = Color.find(params[:id])
-	end
-
-	def color_params
-		params.require(:color).permit(:file)
 	end
 end
--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,2 @@
 class Color < ActiveRecord::Base
-	has_attached_file :file
-	validates_attachment_file_name :file, matches: [/tmTheme\Z/]
 end
